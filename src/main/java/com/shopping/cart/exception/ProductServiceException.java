aea8617d3578597c4533efa0dfd520feb0b124d0
basil5558 <108655862+basil5558@users.noreply.github.com>
2023-05-04 13:19:09 +0530

    Update ProductServiceException.java

--- src/main/java/com/shopping/cart/exception/ProductServiceException.java
+++ src/main/java/com/shopping/cart/exception/ProductServiceException.java
@@ -1,12 +1,11 @@
 package com.shopping.cart.exception;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 
 public class ProductServiceException extends RuntimeException {
     String errorMessage;
-    HttpStatusCode httpStatus;
-    public ProductServiceException(String errorMessage, HttpStatusCode httpStatusCode){
+    HttpStatus httpStatus;
+    public ProductServiceException(String errorMessage, HttpStatus httpStatusCode){
         super(errorMessage);
         this.errorMessage = errorMessage;
         this.httpStatus = httpStatusCode;
