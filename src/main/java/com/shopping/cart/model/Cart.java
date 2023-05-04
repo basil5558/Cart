2da3a8faef4d623ffc1236346ce5c9aa63ad24b0
basil5558 <108655862+basil5558@users.noreply.github.com>
2023-05-04 12:35:47 +0530

    Update Cart.java

--- src/main/java/com/shopping/cart/model/Cart.java
+++ src/main/java/com/shopping/cart/model/Cart.java
@@ -1,15 +1,14 @@
 package com.shopping.cart.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 
 
 @Data
